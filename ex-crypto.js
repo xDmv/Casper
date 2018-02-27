cf78604fff90166d15d3a549f4c0ed81a7440ccf
Dima Makushin <makushin.dima@gmail.com>
2018-02-27 16:25:38 +0200

    casper 1

---
+++ ex-crypto.js
@@ -0,0 +1,56 @@
+
+var SELECTOR = [{
+  email : '#LoginForm_email',
+  password : '#LoginForm_password',
+  button: '#login-btn',
+  h1 : 'h1',
+}];
+
+var casper = require("casper").create();
+
+// регистрация проверка селекторов и ид
+casper.start('https://test-front.ex-crypto.me/registration', function() {
+  this.echo('Url: ' + this.getCurrentUrl());
+  this.echo('Title: ' + this.getTitle());
+  this.echo('H1: ' + this.exists("h1.text-center.modal-title"));
+  this.echo('h1: ' + this.fetchText('h1'));
+  this.echo('Label: ' + this.exists("label.col-sm-3.control-label.required"));
+  this.echo('Label: ' + this.fetchText('label.col-sm-3.control-label.required'));
+  this.echo('button: ' + this.exists('#yw0'));
+  this.echo('button: ' + this.fetchText('#yw0'));
+  casper.capture('registration1.png');
+  console.log('Сохранен скриншот: registration1.png');
+// регестрация делаем регистрацию email: avtospam1@gmail.com password: DimaOk5#!
+
+this.echo('checked: ' + this.fetchText('div.float-form.form-group.login-cont.agreement-group.col-sm-12.has-error'));
+/*
+  this.test.assertExists('input#RegistrationForm_offerta');
+  this.click('input#RegistrationForm_offerta');
+  this.wait(100, function() {
+    this.test.assert(this.evaluate(function () {
+      return document.getElementById('RegistrationForm_offerta').checked;
+    }));
+  });
+*/
+  this.fill('form[action="/registration"]', { 'RegistrationForm[email]': 'avtospam1@gmail.com', 'RegistrationForm[password]': 'DimaOk5#!'}, true);
+  casper.capture('registration2.png');
+  console.log('Сохранен скриншот: registration2.png');
+});
+
+
+/*
+casper.start('https://test-front.ex-crypto.me/registration', function() {
+  this.echo('Url: ' + this.getCurrentUrl());
+  this.echo('Title: ' + this.getTitle());
+  this.fill('form[action="/registration"]', { 'RegistrationForm[email]': 'avtospam1@gmail.com', 'RegistrationForm[password]': 'DimaOk5#!'}, true);
+  casper.capture('registration2.png');
+});
+
+/*
+casper.start('http://mobilka.net.ua/admin/', function() {
+  this.fill('form[action="http://mobilka.net.ua/admin/index.php?route=common/login"]', { '#input-username': 'DimaOk', '#input-password': '1983DimaOk5'}, true);
+  this.captureSelector('capture.png');
+});
+*/
+
+casper.run();
---
+++ test.js
@@ -0,0 +1,92 @@
+/*
+var casper = require("casper").create()
+
+casper.start('https://www.google.com/', function() {
+  this.echo('Page url is ' + this.getCurrentUrl());
+  this.echo('Page title is ' + this.getTitle());
+//  this.fill('form[action="/search"]', { 'q': 'casperjs' }, true);
+});
+
+/*
+casper.then(function() {
+    this.echo('Page url is ' + this.getCurrentUrl());
+    this.echo('Page title is ' + this.getTitle());
+});
+*/
+/*
+casper.run();
+
+/*
+var links = [];
+var casper = require('casper').create();
+
+function getLinks() {
+    var links = document.querySelectorAll('h3.r a');
+    return Array.prototype.map.call(links, function(e) {
+        return e.getAttribute('href');
+    });
+}
+
+casper.start('http://google.fr/', function() {
+   // Wait for the page to be loaded
+   this.waitForSelector('form[action="/search"]');
+});
+
+
+casper.then(function() {
+   // search for 'casperjs' from google form
+   this.fill('form[action="/search"]', { q: 'casperjs' }, true);
+});
+
+casper.then(function() {
+    // aggregate results for the 'casperjs' search
+    links = this.evaluate(getLinks);
+    // now search for 'phantomjs' by filling the form again
+    this.fill('form[action="/search"]', { q: 'phantomjs' }, true);
+});
+
+casper.then(function() {
+    // aggregate results for the 'phantomjs' search
+    links = links.concat(this.evaluate(getLinks));
+});
+
+
+casper.run(function() {
+    // echo results in some pretty fashion
+    this.echo(links.length + ' links found:');
+    this.echo(' - ' + links.join('\n - ')).exit();
+});
+
+
+/*
+var casper = require("casper").create()
+
+casper.start('https://test-front.ex-crypto.me/', function() {
+
+});
+casper.then(function() {
+    this.echo('Page url: ' + this.getCurrentUrl());
+    this.echo('Page title: ' + this.getTitle());
+});
+
+casper.run();
+*/
+var casper = require("casper").create();
+
+casper.start('http://mobilka.net.ua/admin/', function() {
+  this.fill('form[action="http://mobilka.net.ua/admin/index.php?route=common/login"]', { 'username': 'DimaOk', 'password': '1983DimaOk5'}, true);
+  this.captureSelector('capture.png', '.navbar-brand');
+
+});
+casper.thenClick('.fa-key');
+
+casper.then(function() {
+    this.echo('Page url is ' + this.getCurrentUrl());
+    this.echo('Page title is ' + this.getTitle());
+});
+
+
+casper.thenClick('.fa-folder-open');
+// casper.thenClick('.fa-folder-open');
+
+casper.run();
